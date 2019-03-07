4f3ce82526d3f9256eec0d1f82e8b8a81ce34d39
André Rodrigues da Silva <andre.002016@gmail.com>
2019-03-07 00:02:17 -0300

    Configuração por json

--- FileWatcher/DbUtils.cs
+++ FileWatcher/DbUtils.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using ForLogic.Persistence;
 namespace FileWatcher
 {
@@ -11,6 +13,13 @@ namespace FileWatcher
         public static string ConnectionString { get; set; } = @"Data Source=localhost; Initial Catalog=qualiex; Integrated Security=false; User Id=sa; Password=abc123##;";
         public static void SetDbParameters()
         {
+            using(var sr = new StreamReader("config.json"))
+            {
+                var content = sr.ReadToEnd();
+                var serializer = new JavaScriptSerializer();
+                var dyn = serializer.Deserialize<Dictionary<string,object>>(content);
+                ConnectionString = dyn["ConnectionString"].ToString();
+            }
             Db.ShowConsoleSql = true;
             Db.ReplaceDbInstructions = false;
             Db.SetDbProvider(EDbProvider.SqlServer);
